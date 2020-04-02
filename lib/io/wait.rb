294654d0d3a9d72bad1adcb4d3c0c4dbaaa45b47
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2020-04-02 16:22:36 +0900

    bundle gem io-wait

---
+++ lib/io/wait.rb
@@ -0,0 +1,8 @@
+require "io/wait/version"
+
+module Io
+  module Wait
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/io/wait/version.rb
@@ -0,0 +1,5 @@
+module Io
+  module Wait
+    VERSION = "0.1.0"
+  end
+end
